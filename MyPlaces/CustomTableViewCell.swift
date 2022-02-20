988dfb02b5f40ecc6d81b0207848059f4b22f630
4tknox <kolundaevva@gmail.com>
2022-02-20 14:11:10 +0300

    Custom Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Владислав Колундаев on 20.02.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+  @IBOutlet var imageOfPlace: UIImageView!
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var locationLabel: UILabel!
+  @IBOutlet var typeLabel: UILabel!
+  
+}
--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -27,15 +27,11 @@ class MainViewController: UITableViewController {
   }
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    
-    var content = cell.defaultContentConfiguration()
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
 
-    content.text = restaurantNames[indexPath.row]
-    content.image = UIImage(named: restaurantNames[indexPath.row])
-    content.imageProperties.cornerRadius = cell.frame.size.height / 2
-    
-    cell.contentConfiguration = content
+    cell.nameLabel.text = restaurantNames[indexPath.row]
+    cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+    cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
     
     return cell
   }
